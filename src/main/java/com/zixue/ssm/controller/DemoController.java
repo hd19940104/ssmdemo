4d6cb721a7e82c35825be6bb82198b23779664b0
hd19940104 <952542927@qq.com>
2020-09-06 11:20:25 +0800

    update

--- src/main/java/com/zixue/ssm/controller/DemoController.java
+++ src/main/java/com/zixue/ssm/controller/DemoController.java
@@ -1,9 +1,6 @@
 package com.zixue.ssm.controller;
 
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
