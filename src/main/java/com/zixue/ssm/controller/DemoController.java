5861e6e9ab4a9d229f73a113d02b0a55190a89fa
hd19940104 <952542927@qq.com>
2020-09-06 12:39:00 +0800

    add swagger

--- src/main/java/com/zixue/ssm/controller/DemoController.java
+++ src/main/java/com/zixue/ssm/controller/DemoController.java
@@ -1,17 +1,19 @@
 package com.zixue.ssm.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Api(value = "测试类")
 public class DemoController {
 
     @RequestMapping("/demo")
 //    @ResponseBody
-
+@ApiOperation(value = "demo",notes = "这是一个测试接口",httpMethod = "GET")
     public  String demo(){
         return "this is demo()";
     }
